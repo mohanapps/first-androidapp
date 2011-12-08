2c3ad2aa7658c51b15417be547f238e1fcdb77d6
rdino.mohan@gmail.com <rdino.mohan@gmail.com@15344a18-97db-7660-d511-10a788f72745>
2011-12-08 20:26:56 +0000

    Initial import.

---
+++ trunk/first-androidapp/HelloAndroid/bin/HelloAndroidActivity.ap_.d
@@ -0,0 +1,10 @@
+C:\mohan\RND\workspace\Android\HelloAndroid\bin\HelloAndroidActivity.ap_ \
+ : C:\mohan\RND\workspace\Android\HelloAndroid\res\drawable-hdpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\res\drawable-ldpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\res\drawable-mdpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\res\layout\main.xml \
+C:\mohan\RND\workspace\Android\HelloAndroid\res\values\strings.xml \
+C:\mohan\RND\workspace\Android\HelloAndroid\bin\res\drawable-hdpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\bin\res\drawable-ldpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\bin\res\drawable-mdpi\ic_launcher.png \
+C:\mohan\RND\workspace\Android\HelloAndroid\AndroidManifest.xml \
